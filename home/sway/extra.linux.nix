6f80383c807b39532fbf18200fefbfaffad5d85b
Kevin Fisher <kfish610@gmail.com>
2023-10-01 22:19:35 -0700

    Fix formatting of hm systemd service

--- home/sway/extra.linux.nix
+++ home/sway/extra.linux.nix
@@ -5,16 +5,20 @@
   home.packages = [ pkgs.polkit_gnome ];
   systemd = {
     user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
+      Unit = {
+        Description = "polkit-gnome-authentication-agent-1";
+        Wants = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+      };
+      Service = {
         Type = "simple";
         ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
         Restart = "on-failure";
         RestartSec = 1;
         TimeoutStopSec = 10;
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
       };
     };
   };
