bbe24ac695bca461ea7aad5e6f3d2d2d3a5785d2
Kevin Fisher <kfish610@gmail.com>
2024-02-12 15:49:46 -0800

    Add comments

--- home/zsh/zsh.nix
+++ home/zsh/zsh.nix
@@ -9,18 +9,6 @@
     enableAutosuggestions = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-    initExtraFirst = ''
-      if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
-        source ~/.p10k.zsh
-      else
-        exec bash
-      fi
-    '';
-    initExtra = ''
-      unsetopt EXTENDED_GLOB
-      npm set prefix ~/.npm-global
-      eval "$(micromamba shell hook --shell=zsh)"
-    '';
     sessionVariables = {
       PATH = "$PATH:~/.npm-global/bin:~/.cargo/bin";
     };
@@ -30,15 +18,24 @@
         "romkatv/powerlevel10k"
       ];
     };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "sudo"
-        "history"
-        "dirhistory"
-        "gh"
-        "npm"
-      ];
-    };
+    initExtraFirst = ''
+      # Load powerline10k only if the terminal supports it
+      # Otherwise just use bash
+      if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
+        source ~/.p10k.zsh
+      else
+        exec bash
+      fi
+    '';
+    initExtra = ''
+      # Turn of extended glob, it is mostly useless and clashes with flakes
+      unsetopt EXTENDED_GLOB
+
+      # Set NPM global in the user directory so it doesn't clash with Nix
+      npm set prefix ~/.npm-global
+
+      # Set up micromamba
+      eval "$(micromamba shell hook --shell=zsh)"
+    '';
   };
 }
