8f9d4f3337e6cb898fb9f33ff8265662098be059
Kevin Fisher <kfish610@gmail.com>
2025-07-15 19:23:58 -0700

    Modify ndk root and add nix ld, tweaks for flutter

--- home/packages/programming.nix
+++ home/packages/programming.nix
@@ -2,36 +2,46 @@
 
 let
   buildToolsVersion = "35.0.0";
+  ndkVersion = "28.1.13356709";
   androidComposition = pkgs.androidenv.composeAndroidPackages {
-    includeEmulator = true;
     includeNDK = true;
+    includeEmulator = true;
+    includeSystemImages = true;
+    systemImageTypes = [
+      "google_apis"
+      "google_apis_playstore"
+    ];
     buildToolsVersions = [ buildToolsVersion ];
-    ndkVersion = "28.1.13356709";
+    cmdLineToolsVersion = "8.0";
+    ndkVersion = ndkVersion;
     platformVersions = [ "35" ];
     abiVersions = [
       "x86_64"
       "armeabi-v7a"
       "arm64-v8a"
     ];
+    useGoogleAPIs = true;
     extraLicenses = [
       "android-googletv-license"
       "android-sdk-arm-dbt-license"
+      "android-sdk-license"
       "android-sdk-preview-license"
       "google-gdk-license"
+      "intel-android-extra-license"
+      "intel-android-sysimage-license"
       "mips-android-sysimage-license"
     ];
   };
 
   ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
-  ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk-bundle";
-  GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
 in
 {
   programs.zsh.sessionVariables = {
     ANDROID_HOME = ANDROID_HOME;
     ANDROID_SDK_ROOT = ANDROID_HOME;
-    ANDROID_NDK_ROOT = ANDROID_NDK_ROOT;
-    GRADLE_OPTS = GRADLE_OPTS;
+    ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk/${ndkVersion}";
+    GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
+    QT_QPA_PLATFORM = "wayland;xcb";
   };
 
   home.packages =
