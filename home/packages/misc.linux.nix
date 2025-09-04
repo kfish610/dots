6fef6a74da0de5a795e152d806f1f5bab8a84fd8
Kevin Fisher <kfish610@gmail.com>
2025-09-03 22:29:22 -0500

    Make wonderdraft optional

--- home/packages/misc.linux.nix
+++ home/packages/misc.linux.nix
@@ -3,42 +3,48 @@
 {
   fonts.fontconfig.enable = true;
 
-  home.packages = with pkgs; [
-    brightnessctl
-    discord
-    gparted
-    # musescore
-    openconnect
-    remmina
-    slack
-    sway-contrib.grimshot
-    vscode
-    zoom-us
-    # postman
-    teams-for-linux
-    networkmanager-openconnect
+  home.packages =
+    with pkgs;
+    [
+      brightnessctl
+      discord
+      gparted
+      # musescore
+      openconnect
+      remmina
+      slack
+      sway-contrib.grimshot
+      vscode
+      zoom-us
+      # postman
+      teams-for-linux
+      networkmanager-openconnect
 
-    godot_4
-    gimp
+      godot_4
+      gimp
 
-    rnote
+      rnote
 
-    obsidian
+      obsidian
 
-    # nix store add --hash-algo sha256 --mode flat ~/Downloads/Wonderdraft-1.1.8.2b-Linux64.deb
-    wonderdraft
-
-    # Fonts
-    dejavu_fonts
-    nerd-fonts.fira-code
-    nerd-fonts.fira-mono
-    nerd-fonts.sauce-code-pro
-    source-code-pro
-    material-design-icons
-    font-awesome
-  ];
-
-  # programs.discocss = {
-  #   enable = true;
-  # };
+      # Fonts
+      dejavu_fonts
+      nerd-fonts.fira-code
+      nerd-fonts.fira-mono
+      nerd-fonts.sauce-code-pro
+      source-code-pro
+      material-design-icons
+      font-awesome
+    ]
+    ++
+      # nix store add --hash-algo sha256 --mode flat ~/Downloads/Wonderdraft-1.1.8.2b-Linux64.deb
+      (
+        let
+          wonderdraftDebPath = "/nix/store/whg8cp587xr93lmdv21qj999y34bs0in-Wonderdraft-1.1.8.2b-Linux64.deb";
+        in
+        if builtins.pathExists wonderdraftDebPath then
+          [ wonderdraft ]
+        else
+          builtins.trace "Warning: Wonderdraft .deb not found, skipping package." [ ]
+      );
 }
