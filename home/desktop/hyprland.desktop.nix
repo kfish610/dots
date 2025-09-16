066b0f7efe1f3919a1f1c802ffca4bc568d0f12e
Kevin Fisher <kfish610@gmail.com>
2025-09-16 11:49:39 -0500

    Change auto direction until I can upgrade hyprland

--- home/desktop/hyprland.desktop.nix
+++ home/desktop/hyprland.desktop.nix
@@ -3,6 +3,6 @@
 {
   wayland.windowManager.hyprland.settings.monitor = [
     "DP-2, highrr, auto, 1"
-    "DP-4, highrr, auto-center-right, 1, vrr, 1, transform, 3"
+    "DP-4, highrr, auto-right, 1, vrr, 1, transform, 3" # TODO: Change to auto-center-right when I can upgrade to unstable
   ];
 }
