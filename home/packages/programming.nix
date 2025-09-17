1071e2b1ed603e18a743b9f6c1a26ab2cd9be4d2
Kevin Fisher <kfish610@gmail.com>
2025-09-17 11:07:38 -0500

    Remove R

--- home/packages/programming.nix
+++ home/packages/programming.nix
@@ -42,89 +42,66 @@ in
     ANDROID_HOME = ANDROID_HOME;
     ANDROID_SDK_ROOT = ANDROID_HOME;
     ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk/${ndkVersion}";
-    # GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
     QT_QPA_PLATFORM = "wayland;xcb";
   };
 
-  home.packages =
-    with pkgs;
-    [
-      # Agda
-      (agda.withPackages (
-        p: with p; [
-          standard-library
-        ]
-      ))
-
-      # C/C++
-      gnumake
-      cmake
-      gcc
-
-      # Clojure
-      clojure
-      leiningen
-      babashka
-
-      # Coq
-      coq
-      # coqPackages.vscoq-language-server
-      # coqPackages.coq-lsp
-
-      # Exercism
-      exercism
-
-      # Flutter
-      flutter
-      androidComposition.androidsdk
-
-      # Haskell
-      stack
-      haskell-language-server
-
-      # Java/Scala
-      jdk
-      sbt
-
-      # LaTeX
-      texlive.combined.scheme-full
-
-      # Lean
-      elan
-
-      # Nix
-      nil
-      nixfmt-rfc-style
-      devenv
-
-      # Node
-      nodejs_latest
-
-      # Python
-      python312
-      micromamba
-
-      # Qt
-      qt6.qtdeclarative
-    ]
-    ++ (
-      let
-        rOverride = {
-          packages = with pkgs.rPackages; [
-            # For the editor
-            languageserver
-            # httpgd
-            rmarkdown
-
-            # Tidyverse
-            tidyverse
-            tidymodels
-          ];
-        };
-      in
-      [
-        (rWrapper.override rOverride)
-        (radianWrapper.override rOverride)
+  home.packages = with pkgs; [
+    # Agda
+    (agda.withPackages (
+      p: with p; [
+        standard-library
       ]
-    );
+    ))
+
+    # C/C++
+    gnumake
+    cmake
+    gcc
+
+    # Clojure
+    clojure
+    leiningen
+    babashka
+
+    # Coq
+    coq
+    # coqPackages.vscoq-language-server
+    # coqPackages.coq-lsp
+
+    # Exercism
+    exercism
+
+    # Flutter
+    flutter
+    androidComposition.androidsdk
+
+    # Haskell
+    stack
+    haskell-language-server
+
+    # Java/Scala
+    jdk
+    sbt
+
+    # LaTeX
+    texlive.combined.scheme-full
+
+    # Lean
+    elan
+
+    # Nix
+    nil
+    nixfmt-rfc-style
+    devenv
+
+    # Node
+    nodejs_latest
+
+    # Python
+    python312
+    micromamba
+
+    # Qt
+    qt6.qtdeclarative
+  ];
 }
