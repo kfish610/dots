959e52536b171246f048bbcdb71d03a6abfc0f4c
Kevin Fisher <kfish610@gmail.com>
2025-09-21 17:19:11 -0500

    Open discord and chrome in the right place

--- home/desktop/niri.desktop.nix
+++ home/desktop/niri.desktop.nix
@@ -1,20 +1,40 @@
 { ... }:
 
 {
-  programs.niri.settings.outputs = {
-    "DP-2".mode = {
-      width = 2560;
-      height = 1440;
-      refresh = 180.000;
-    };
-    "DP-4" = {
-      mode = {
-        width = 1920;
-        height = 1080;
-        refresh = 165.003;
+  programs.niri.settings = {
+    outputs = {
+      "DP-2".mode = {
+        width = 2560;
+        height = 1440;
+        refresh = 180.000;
       };
-      transform.rotation = 270;
-      variable-refresh-rate = true;
+      "DP-4" = {
+        mode = {
+          width = 1920;
+          height = 1080;
+          refresh = 165.003;
+        };
+        transform.rotation = 270;
+        variable-refresh-rate = true;
+      };
     };
+
+    window-rules = [
+      {
+        matches = [
+          {
+            app-id = "Google-chrome";
+            at-startup = true;
+          }
+          {
+            app-id = "discord";
+            at-startup = true;
+          }
+        ];
+
+        open-maximized = true;
+        open-on-output = "DP-4";
+      }
+    ];
   };
 }
