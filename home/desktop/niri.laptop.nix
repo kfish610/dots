fae38e0b1665f83b8df416e8cbf6dcaec844831f
Kevin Fisher <kfish610@gmail.com>
2025-09-23 12:57:30 -0500

    Force no scaling on eDP-1 for laptop (otherwise everything looks awful)

--- home/desktop/niri.laptop.nix
+++ home/desktop/niri.laptop.nix
@@ -1,12 +1,18 @@
 { pkgs, ... }:
 
 {
-  programs.niri.settings.spawn-at-startup = [
-    {
-      argv = [
-        "${pkgs.rot8}/bin/rot8"
-        "-k"
-      ];
-    }
-  ];
+  programs.niri.settings = {
+    outputs = {
+      "eDP-1".scale = 1;
+    };
+
+    spawn-at-startup = [
+      {
+        argv = [
+          "${pkgs.rot8}/bin/rot8"
+          "-k"
+        ];
+      }
+    ];
+  };
 }
