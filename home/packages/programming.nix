309bd588b7ceb049454b40e2c64f2cdd7a5328f6
Kevin Fisher <kfish610@gmail.com>
2025-10-13 19:41:00 -0500

    Remove flutter (it works better in a dev container)

--- home/packages/programming.nix
+++ home/packages/programming.nix
@@ -1,47 +1,7 @@
 { pkgs, ... }:
 
-let
-  buildToolsVersion = "34.0.0";
-  ndkVersion = "26.3.11579264";
-  androidComposition = pkgs.androidenv.composeAndroidPackages {
-    includeNDK = true;
-    includeCmake = true;
-    includeEmulator = true;
-    includeSystemImages = true;
-    systemImageTypes = [
-      "google_apis"
-      "google_apis_playstore"
-    ];
-    buildToolsVersions = [ buildToolsVersion ];
-    cmdLineToolsVersion = "8.0";
-    cmakeVersions = [ "3.22.1" ];
-    ndkVersion = ndkVersion;
-    platformVersions = [ "35" ];
-    abiVersions = [
-      "x86_64"
-      "armeabi-v7a"
-      "arm64-v8a"
-    ];
-    useGoogleAPIs = true;
-    extraLicenses = [
-      "android-googletv-license"
-      "android-sdk-arm-dbt-license"
-      "android-sdk-license"
-      "android-sdk-preview-license"
-      "google-gdk-license"
-      "intel-android-extra-license"
-      "intel-android-sysimage-license"
-      "mips-android-sysimage-license"
-    ];
-  };
-
-  ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
-in
 {
   programs.zsh.sessionVariables = {
-    ANDROID_HOME = ANDROID_HOME;
-    ANDROID_SDK_ROOT = ANDROID_HOME;
-    ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk/${ndkVersion}";
     QT_QPA_PLATFORM = "wayland;xcb";
   };
 
@@ -70,10 +30,6 @@ in
 
     # Exercism
     exercism
-
-    # Flutter
-    flutter
-    androidComposition.androidsdk
 
     # Haskell
     stack
