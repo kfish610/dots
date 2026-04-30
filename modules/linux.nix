650e8f55a48a93e9b9097a751771e75b0e645451
Kevin Fisher <kfish610@gmail.com>
2026-04-30 12:28:44 -0500

    Use systemd-resolved

--- modules/linux.nix
+++ modules/linux.nix
@@ -12,8 +12,25 @@
   networking.networkmanager = {
     enable = true;
     plugins = [ pkgs.networkmanager-openconnect ];
+    dns = "systemd-resolved";
   };
   users.groups.networkmanager.members = [ "kfish" ];
+
+  # Use systemd-resolved for DNS resolution
+  networking.nameservers = [
+    "1.1.1.1#one.one.one.one"
+    "1.0.0.1#one.one.one.one"
+  ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+    ];
+    dnsovertls = "true";
+  };
 
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
